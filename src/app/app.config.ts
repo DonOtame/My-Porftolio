8118e159d2ca16d4142ef41ece98f3f354f3732b
Danny <danny_pichincha@hotmail.com>
2025-09-05 11:57:38 -0500

    feat: integrate ngx-translate and flowbite, update styles and environment files
    - Added ngx-translate/core and ngx-translate/http-loader for internationalization support.
    - Integrated Flowbite for UI components.
    - Updated app configuration to include translation service and initialization logic.
    - Removed placeholder content from app.html and added Tailwind CSS and Flowbite imports in styles.css.
    - Created environment files for development and production.
    - Deleted obsolete app.spec.ts test file.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,40 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService, TranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { initFlowbite } from 'flowbite';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateService({
+      lang: 'en',
+      fallbackLang: 'en',
+      loader: provideTranslateHttpLoader({
+        prefix: '/i18n/',
+        suffix: '.json',
+      }),
+    }),
+    provideAppInitializer(() => {
+      const translate = inject(TranslateService);
+      const browserLang = translate.getBrowserLang();
+      translate.addLangs(['es', 'en']);
+      translate.setFallbackLang('en');
+      translate.use(browserLang || 'en');
+      initFlowbite();
+    }),
+  ],
 };
