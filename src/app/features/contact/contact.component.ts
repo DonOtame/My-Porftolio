efd56ffcbdb947fd4168574837e743531cc763ef
Danny <danny_pichincha@hotmail.com>
2025-09-07 12:18:19 -0500

    feat: enhance contact component with dynamic links and email copy functionality

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,9 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { bootstrapClipboard2, bootstrapGithub, bootstrapLinkedin } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [TranslatePipe, NgIcon],
+  viewProviders: [provideIcons({ bootstrapGithub, bootstrapLinkedin, bootstrapClipboard2 })],
   templateUrl: './contact.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ContactComponent {}
+export default class ContactComponent {
+  private translate = inject(TranslateService);
+
+  copyEmail(emailKey: string) {
+    const email = this.translate.instant(emailKey);
+    navigator.clipboard
+      .writeText(email)
+      .then(() => {
+        console.log('Correo copiado al portapapeles');
+      })
+      .catch((err) => {
+        console.error('Error al copiar correo:', err);
+      });
+  }
+}
